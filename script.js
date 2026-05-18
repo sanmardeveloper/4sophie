7ce2b4a1c6dfc060f76a24b64fc1d2f28d58b737
Sanmar Official <sanmaroff@gmail.com>
2026-05-18 23:44:33 +0300

    Доработал отпарвку

--- script.js
+++ script.js
@@ -148,8 +148,11 @@ async function openGift(day) {
     let responseData = null;
 
     try {
-        const response = await fetch(`https://3sja43qage.eu.loclx.io /get_day?day_id=${day}`, {
-            method: "GET"
+        const response = await fetch(`https://3sja43qage.eu.loclx.io/get_day?day_id=${day}`, {
+            method: "GET",
+            headers: {
+                "X-LocalXpose-Skip-Warning": "true"
+            }
         });
 
         if (response.ok) {
