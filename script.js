af7f85cf6de0df2e67b780aec935bd6f6678f4c8
Sanmar Official <sanmaroff@gmail.com>
2026-05-18 21:57:47 +0300

    changed response on openGift()

--- script.js
+++ script.js
@@ -148,12 +148,10 @@ async function openGift(day) {
     let responseData = null;
 
     try {
-        const response = await fetch(`https://a8f8-62-84-98-60.ngrok-free.app/get_day?day_id=${day}`, {
-    method: "GET",
-    headers: {
-        "ngrok-skip-browser-warning": "true"
-    }
-});
+        const response = await fetch(`https://a8f8-62-84-98-60.ngrok-free.app/get_day?day_id=${day}&ngrok-skip-browser-warning=true`, {
+            method: "GET"
+        });
+
         if (response.ok) {
             responseData = await response.json();
             console.log("Данные за день получены:", responseData);
